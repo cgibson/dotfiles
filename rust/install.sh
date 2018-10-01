23f856f242bd2bfd15b7b8bea2fe2a051d75c00a
Chris Gibson <cgibson@mrvoxel.com>
2018-09-30 18:47:33 -0700

    Added rust install.sh (rustup) script and path.zsh

---
+++ rust/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# Rust
+#
+# This installs rustup
+
+echo "› checking rustup install"
+
+SHOULD_INSTALL=false
+
+if ! [ -d $HOME/.cargo ]
+then
+  SHOULD_INSTALL=true
+else
+  source $HOME/.cargo/env
+  if test ! $(which rustup)
+  then
+  	SHOULD_INSTALL=true
+  fi
+fi
+
+if $SHOULD_INSTALL
+then
+  echo "  Installing rustup for you."
+
+  curl https://sh.rustup.rs -sSf | sh
+fi
